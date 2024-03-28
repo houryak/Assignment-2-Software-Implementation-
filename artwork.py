ef27031acf40ef7d4c47c6eeae560db6b6986a71
houryak <160161854+houryak@users.noreply.github.com>
2024-03-29 00:05:56 +0400

    Add files via upload

--- artwork.py
+++ artwork.py
@@ -1,6 +1,5 @@
 class Artwork:
     def __init__(self, title, artist, date, significance, location):
-        # Initialize the attributes of the Artwork class
         self.title = title
         self.artist = artist
         self.date = date
@@ -8,5 +7,4 @@ class Artwork:
         self.location = location
 
     def get_info(self):
-        # Return a formatted string containing information about the artwork
         return f"{self.title} by {self.artist} created on {self.date}. Significance: {self.significance} at {self.location}"
